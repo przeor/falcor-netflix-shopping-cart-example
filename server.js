04a626dfbd6f94654df02aab17b05ed5a26739ae
przeor <kamil@przeorski.pl>
2016-02-15 14:44:35 +0100

    init

---
+++ server.js
@@ -0,0 +1,26 @@
+var falcorExpress = require('falcor-express');
+var Router = require('falcor-router');
+
+var express = require('express');
+var app = express();
+
+app.use('/model.json', falcorExpress.dataSourceRoute(function (req, res) {
+  // create a Virtual JSON resource with single key ("greeting")
+  return new Router([
+    {
+      // match a request for the key "greeting"
+      route: "greeting",
+      // respond with a PathValue with the value of "Hello World."
+      get: function() {
+        return {path:["greeting"], value: "Hello World2"};
+      }
+    }
+  ]);
+}));
+
+
+// serve static files from current directory
+app.use(express.static(__dirname + '/'));
+
+app.listen(3001);
+console.log("Listening on http://localhost:3001");
