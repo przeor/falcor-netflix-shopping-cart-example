80fda570f6c9e113fa1ce060afc940dc6ea68140
przeor <kamil@przeorski.pl>
2016-02-16 15:50:31 +0100

    moving Falcor model from client to backend side, model.asDataSource

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
 var falcorExpress = require('falcor-express');
 var Router = require('falcor-router');
-
+var falcor = require('falcor');
 var express = require('express');
 var app = express();
 
-app.use('/model.json', falcorExpress.dataSourceRoute(function (req, res) {
-  // create a Virtual JSON resource with single key ("greeting")
-  return new Router([
-    {
-      // match a request for the key "greeting"
-      route: "greeting",
-      // respond with a PathValue with the value of "Hello World."
-      get: function() {
-        return {path:["greeting"], value: "Hello World2"};
-      }
-    }
-  ]);
-}));
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+var $ref = falcor.Model.ref;
+var model = new falcor.Model({
+  cache: {
+      productsById: {
+           1: {
+               name: "Product ABC from backend",
+               otherAdd: "something 1"
+           },
+
+      },
+      _view: [ $ref('productsById[1]') ],
+      _cart: []
+  }
+});
 
 
 // serve static files from current directory
 app.use(express.static(__dirname + '/'));
+
+app.use('/model.json', falcorExpress.dataSourceRoute(function(req, res) {
+  // data source interface goes here
+  return model.asDataSource();
+}))
+
+
+
 
 app.listen(3001);
 console.log("Listening on http://localhost:3001");
